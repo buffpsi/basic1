45d71a629922b9302364ff34e49854596f7cdbcc
buffpsi <rlawlstjd778@naver.com>
2023-03-14 11:05:35 +0900

    first page

--- src/main/java/com/ll/basic/HomeController.java
+++ src/main/java/com/ll/basic/HomeController.java
@@ -1,2 +1,14 @@
-package com.ll.basic;public class HomeController {
+package com.ll.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String showMain(){
+        return "안녕하세요";
+    }
 }
